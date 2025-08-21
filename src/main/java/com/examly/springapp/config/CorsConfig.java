153d4f8db6af627cdede8500380fb9318a3aecd1
Aadharsh1006 <aadharshrameish@gmail.com>
2025-08-22 01:39:43 +0530

    Ready to deploy in Railway.

--- src/main/java/com/examly/springapp/config/CorsConfig.java
+++ src/main/java/com/examly/springapp/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://blog-management-frontend-nine.vercel.app"));
+        configuration.setAllowedOrigins(List.of("https://blog-management-frontend-nine.vercel.app/"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
